3be952b1575060c6b7266e026045e0656d01fc6c
vboxuser <n_timchuk@bk.ru>
2024-04-11 19:30:41 +0300

    blog_start

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,9 @@
-
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponseRedirect, HttpResponseNotAllowed
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, TemplateView, CreateView
-from django.contrib import messages
-
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, FormView
 
 from .models import Post
 from .forms import RegisterUserForm, LoginUserForm
@@ -28,17 +23,6 @@ class BlogList(ListView):
         context['form'] = RegisterUserForm()
         return context
 
-    #def get(self, request, *args, **kwargs):
-    #    form = RegisterUserForm()
-    #    return render(request, self.template_name, {'form': form})
-    #
-    #def post(self, request):
-    #    form = RegisterUserForm(request.POST)
-    #    if form.is_valid():
-    #        form.save()
-    #        return redirect('login')
-    #    return render(request, self.template_name, {'form': form})
-
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
@@ -48,27 +32,23 @@ class AboutPageView(TemplateView):
     template_name = 'about.html'
 
 
-class RegisterUserView(CreateView):
+class RegisterUserView(FormView):
     form_class = RegisterUserForm
     template_name = 'register.html'
-    success_url = ''
-    def get(self, request):
-        form = RegisterUserForm
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         form.save()
-        return super().form_valid(form)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return super().form_valid(form)  # Возвращаем JSON-ответ о успешной регистрации
 
-    def form_invalid(self, form):
-        return HttpResponseNotAllowed(permitted_methods=['POST'])
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = self.form_class()
+        return context
 
 
 class LoginUser(LoginView):
